4faa7f10d07e332780fa0113585bb778c03044fd
pralhad7398 <87436669+pralhad7398@users.noreply.github.com>
2026-03-20 13:15:03 +0530

    Update server to listen on all network interfaces

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`\n⚡ IssueAI running at http://localhost:${PORT}\n`);
 });
